68af4aecfe6273a67070dfaa0a1f3ec5f25bcb74
jugrajn <jugraj.nijjar@gmail.com>
2021-06-29 21:40:13 -0400

    models created with relationships

---
+++ models/index.js
@@ -0,0 +1,34 @@
+const User = require('./user.js');
+const Recipe = require('./recipe.js');
+const Cookbook = require('./cookbook.js');
+
+// SET UP RELATIONSHIPS BETWEEN MODELS
+User.hasMany(Recipe, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade'
+});
+
+User.hasOne(Cookbook, {
+    foriegnKey: 'user_id'
+});
+
+Cookbook.hasMany(Recipe, {
+    foreighKey: 'cookbook_id',
+    onDelete: 'cascade'
+});
+
+Cookbook.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Recipe.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade'
+});
+
+Recipe.belongsTo(Cookbook, {
+    foreignKey: 'cookbook_id',
+    onDelete: 'cascade'
+});
+
+modeul.exports = { User, Recipe, Cookbook};
