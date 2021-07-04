e278a4fab4d972564140e0ec26063c15d189658b
AnaMolly <analia.mohamdally@gmail.com>
2021-07-04 15:15:00 -0400

    adjusted js files, models, handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
+const userRoutes = require('./user-routes');
 const dataRoutes = require('./data-routes');
 const recipeRoutes = require('./search-recipe');
 const projectRoutes = require('./projectRoutes')
 
-router.use('/users', dataRoutes);
+router.use('/users', userRoutes);
 router.use('/recipes', recipeRoutes);
+router.use('/addRecipes', dataRoutes);
 router.use('/projects', projectRoutes);
+
 
 module.exports = router;
--- public/js/searchrecipe.js
+++ public/js/searchrecipe.js
@@ -1,7 +1,11 @@
 let searchBut = document.querySelector(".search-button")
+let addBut = document.querySelector("#add-to-cookbook")
 let allRecipes = document.querySelector(".all-recipes")
 let recipesListEl = $("#search-results")
 let indRecipe = $("#ind-recipe")
+let recipeIngredients = $("#recipe-ing")
+let recipeInstructions = $("#recipe-ins")
+let individualRecipeEl = $("#recipeEl")
 
 
 
@@ -23,7 +27,7 @@ function getRecipes(){
         console.log(data)
         
         
-        for(let i=0;i<5;i++){
+        for(let i=0;i<6;i++){
 
             recipesListEl.append(
                 `<div class='recipes' id='${data.results[i].id}'  style="display:flex;flex-direction:column; align-items:center; margin:30px"> <h4 style='font-weight:200; font-size:20px; text-align:center; padding:30px'>${data.results[i].title}</h4> <img src='https://spoonacular.com/recipeImages/${data.results[i].image}' style='max-width:200px;max-height:200px'></div>`,
@@ -35,6 +39,8 @@ function getRecipes(){
 
         recipeList.on("click", function(){
             indRecipe.children().remove()
+            recipeIngredients.children().remove()
+            recipeInstructions.children().remove()
             let searchValue = this.id
             getIndRecipe(searchValue)
         })
@@ -65,57 +71,56 @@ function getIndRecipe(searchValue){
         console.log(data)
     
         indRecipe.append(
-            `<div style="display:flex; flex-direction:column; align-items:center; margin:30px"> <h4 id='recipe-title' style='font-weight:200; font-size:40px; text-align:center; padding:30px'>${data.title}</h4> <img class="recipe-id"src='https://spoonacular.com/recipeImages/${data.image}' style='max-width:200px;max-height:200px'></div>`
+            `<h4 id='recipe-title' style='font-weight:200; font-size:40px; text-align:center; padding:30px'>${data.title}</h4> <img id="recipe-img"src='${data.image}' style='max-width:500px;max-height:500px'>`
         )
         
-        indRecipe.append(`<h3> Instructions: </h3>  <p id="recipe-instructions" style="padding: 0px 50px"> ${data.instructions}</p> <br>`)
-        indRecipe.append(`<h3> Ingredients: </h3>`)
+        recipeInstructions.append(`<h3> Instructions: </h3>  <p id="recipe-instructions" style="padding: 0px 50px"> ${data.instructions}</p> <br>`)
+        recipeIngredients.append(`<h3> Ingredients: </h3>`)
        
             for(let i=0;i<30;i++){
-                indRecipe.append(
-                    `  <p id="recipe-ingredients" style="font-size: 20px"> ${data.extendedIngredients[i].original}</p>`
+                recipeIngredients.append(
+                    `  <p class="recipe-ingredients" style="font-size: 20px">${data.extendedIngredients[i].original}</p>`
                 )
             } 
-        
-        
-            
-        
-       
+
     })
     .catch(err => {
         console.error(err);
     });
 }
     
-const addRecipeData = async (event) => {
-    event.preventDefault();
-  
-    // Collect values from the login form
-    const ingredients = document.querySelector('#recipe-ingredients').value.trim();
-    const instructions = document.querySelector('#recipe-instructions').value.trim();
-    const image = document.querySelector('#recipe-img').value.trim();
-  
-    if (username && password) {
-      // Send a POST request to the API endpoint
+const addRecipeData = async () => { 
+    
+    const name = $('#recipe-title').text()
+    const ingredients = $('.recipe-ingredients')
+    const ingredient = ingredients.map(function(){
+        return $(this).text();
+     }).get()
+    const instructions = $('#recipe-instructions').text()
+    const image = $('#recipe-img').attr('src')
+
+    
+    if (name && ingredient && instructions && image) {
+        console.log(ingredient)
       const response = await fetch('/api/recipes/searchRecipe', {
         method: 'POST',
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({name, ingredient, instructions,image}),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        // If successful, redirect the browser to the profile page
-        document.location.replace('/');
+        document.location.replace('/cookbook');
       } else {
         alert(response.statusText);
       }
     }
-  };
+};
 
+addBut.addEventListener("click", function(event){
+    event.preventDefault()
+    addRecipeData()
+})
 
-//   document
-//   .querySelector('.login-form')
-//   .addEventListener('submit', loginFormHandler);
 
 searchBut.addEventListener("click", function(event){
     event.preventDefault()
